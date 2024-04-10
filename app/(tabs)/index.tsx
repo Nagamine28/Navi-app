e13c5e5c35376991c2e97e6f04a7f50cd6ede2bb
yagi-73 <91511994+yagi0409@users.noreply.github.com>
2024-04-10 14:51:11 +0900

    [add]react-native-maps

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,21 @@
-import { StyleSheet } from 'react-native';
+import * as React from 'react';
+import MapView from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+export default function App() {
+  const origin = {latitude: 37.7749, longitude: -122.4194}; // 例としてサンフランシスコ
+  const destination = {latitude: 34.0522, longitude: -118.2437}; // 例としてロサンゼルス
 
-export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <MapView style={styles.map} >
+        <MapViewDirections
+          origin={origin}
+          destination={destination}
+          apikey={'YOUR_API_KEY'}
+        />
+      </MapView>
     </View>
   );
 }
@@ -16,16 +23,12 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
