9f8d5e2ed6ea0a4c40c96cfcc8bc7dbc3393b8e0
enomoto_wsl_laptop <enomoto@example.com>
2024-04-17 15:00:40 +0900

    現在位置へフォーカス

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
-import React, { useState, useEffect } from 'react';
-import MapView ,{Marker} from 'react-native-maps';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
+import MapView, { Marker } from "react-native-maps";
 import MapViewDirections from 'react-native-maps-directions';
-import { StyleSheet, Dimensions } from 'react-native';
+import { StyleSheet, Dimensions, Platform } from "react-native";
 
 import { locationPermission, getCurrentLocation } from '../../helper/helperFunction';
 
@@ -13,6 +13,7 @@ export default function TabOneScreen() {
 
   const origin = {latitude: 37.7749, longitude: -122.4194}; // 例としてサンフランシスコ
   const destination = {latitude: 34.0522, longitude: -118.2437}; // 例としてロサンゼルス
+
 
   const [state, setState] = useState({
     curLoc: {
@@ -47,6 +48,24 @@ export default function TabOneScreen() {
     }
   }
 
+  const ref = useRef<MapView>(null);
+  // effects
+    const onMapReadyHandler = useCallback(() => {
+      if (Platform.OS === 'ios') {
+        ref?.current?.fitToElements(false);
+      } else {
+        ref?.current?.fitToCoordinates([origin, destination], {
+          animated: true,
+          edgePadding: {
+            top: 150,
+            right: 50,
+            bottom: 50,
+            left: 50,
+          },
+        });
+      }
+    }, [ref]);
+
   useEffect(() => {
       getLiveLocation();
   }, []);
@@ -54,18 +73,24 @@ export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-      <Text>Location Permission Status: {permissionStatus}</Text>
-      <Text>Altitude: {altitude}</Text>
-      <MapView style={styles.map} >
-        <Marker coordinate={state.curLoc} />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <View>
+        <Text>Location Permission Status: {permissionStatus}</Text>
+        <Text>Altitude: {altitude}</Text>
+      </View>
+
+      <MapView style={styles.map} onMapReady={onMapReadyHandler} ref={ref}>
+        <Marker coordinate={state.curLoc} identifier={"mk1"} />
         <MapViewDirections
           origin={origin}
           destination={destination}
-          apikey={'YOUR_API_KEY'}
+          apikey={"YOUR_API_KEY"}
         />
-       </MapView>
+      </MapView>
     </View>
   );
 }
@@ -87,6 +112,6 @@ const styles = StyleSheet.create({
   },
   map: {
     width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    height: Dimensions.get('window').height * 0.85,
   },
 });
